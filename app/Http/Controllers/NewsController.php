eb6be3c774d7c6c4fce7f858f654d0a55e9d1d88
Domitory123 <ddmitromaas@gmail.com>
2023-02-06 12:34:49 +0200

    Authorization

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\News;
+use App\Models\User;
+use Illuminate\Foundation\Auth;
+
+class NewsController extends Controller
+{
+    public function submit()
+    {
+      return view('newsblock',['data'=>News::all()]);
+    }
+    public function submitOne($id)
+    {
+          
+     if (\Auth::user()!=null && \Auth::user()->admin)
+     return view('newsblockOne',['data'=>News::find($id)]);
+       else
+     return view('welcome');
+
+       //return view('newsblockOne',['data'=>News::find($id)]);
+    }
+
+
+   
+}
