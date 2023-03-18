3595c58a08e9c5c7fb3e89b04a36c7a4269ced29
Domitory123 <ddmitromaas@gmail.com>
2023-03-18 16:09:32 +0200

    renamed and optimized

--- app/Servisec/Merch/Service.php
+++ app/Servisec/Merch/Service.php
@@ -16,30 +16,23 @@ class Service
     public function store(MerchRequest $request)
     {
       $merch = new Merch();
-      $photoMainName = $request->file('nameMainPhoto')->store('uploads','public');
-      $photoName1 = $request->file('namePhoto1')->store('uploads','public');
-      $photoName2 = $request->file('namePhoto2')->store('uploads','public');
-  
+      $merch = $this->updatePhoto($merch, $request);
       $merch->title= $request->input('title');
       $merch->describe= $request->input('describe');
-      $merch->nameMainPhoto = $photoMainName;
-      $merch->namePhoto1= $photoName1;
-      $merch->namePhoto2= $photoName2;
       $merch->save(); 
     }
 
     public function order(Request $request)
     {
-         $order = new Order();
-         $str= $request->input('idMerch');
-         $order->merch_id=(int)$str;
-         $order->nameUser= $request->input('nameUser');
-         $order->email = $request->input('email');
-         $order->deliveryAddress= $request->input('deliveryAddress');
-         $order->comment= $request->input('comment');
-         $order->phoneNumber= $request->input('phoneNumber');
-         $order->status="оформлення";
-         $order->save();
+      $data = $request->validate([
+        'merch_id' => '',
+        'name_user' => 'string',
+        'email' => 'email',
+        'delivery_address' => 'string',
+        'comment' => 'string',
+        'phone_number' => 'string'
+    ]);
+         Order::create($data);  
     }
 
     public function destroy($id)
@@ -47,40 +40,40 @@ class Service
       $merch = Merch::find($id);
        if($merch)
        { 
-             unlink(public_path('storage\\'.$merch->nameMainPhoto));
-             unlink(public_path('storage\\'.$merch->namePhoto1));
-             unlink(public_path('storage\\'.$merch->namePhoto2));
-             Merch::find($id)->delete();  
+            unlink(public_path('storage\\'.$merch->name_main_photo));
+            unlink(public_path('storage\\'.$merch->name_photo_1));
+            unlink(public_path('storage\\'.$merch->name_photo_2));
+            Merch::find($id)->delete();  
        }
     }
 
     public function update($id, Request $request)
     {
            $merch = Merch::find($id);  
-           if($request->file('nameMainPhoto'))
-           {
-            $photoMainName = $request->file('nameMainPhoto')->store('uploads','public');
-            $merch->nameMainPhoto = $photoMainName;
-           }
-           if($request->file('namePhoto1'))
-           {
-            $photoName1 = $request->file('namePhoto1')->store('uploads','public');
-            $merch->namePhoto1= $photoName1;
-           }
-           if($request->file('namePhoto2'))
-           {
-            $photoName2 = $request->file('namePhoto2')->store('uploads','public');
-            $merch->namePhoto2= $photoName2;
-           }
-           
+           $merch = $this->updatePhoto($merch, $request);
            $merch->title= $request->input('title');
            $merch->describe= $request->input('describe');
            $merch->save();
            return $merch;
-        
     }
 
-
-
-
+    public function updatePhoto(Merch $merch, Request $request)
+    {
+      if($request->file('nameMainPhoto'))
+      {
+        $photoMainName = $request->file('nameMainPhoto')->store('uploads','public');
+        $merch->name_main_photo = $photoMainName;
+      }
+      if($request->file('namePhoto1'))
+      {
+       $photoName1 = $request->file('namePhoto1')->store('uploads','public');
+       $merch->name_photo_1= $photoName1;
+      }
+      if($request->file('namePhoto2'))
+      {
+       $photoName2 = $request->file('namePhoto2')->store('uploads','public');
+       $merch->name_photo_2= $photoName2;
+      }
+      return $merch;
+    }
 }
